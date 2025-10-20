211c216d1b3d5fa1545f8a8221ec77d7169e392d
David Nikolov <Lummotm@proton.me>
2025-10-20 17:19:51 +0200

    ultima version con entrada por defecto de condiciones de convergencia para explicito y print de las graficas

--- practica1/practica1_1.m
+++ practica1/practica1_1.m
@@ -1,15 +1,26 @@
-function [Times,Errors] = practica1_1(choiceMethod, h_values, k_values)
+function [Times,Errors] = practica1_1(choiceMethod, J_values, N_values)
+
+    % Dato del problema
+    T = 0.5;
+
+    % Condiciones iniciales y solución exacta
+    u0_fun = @(x) sin(pi*x);
+    u_exact = @(x,t) sin(pi*x) .* exp(-pi^2 * t);
+
+    % Gestión de inputs
     if nargin < 1 || isempty(choiceMethod)
         choiceMethod = 0;
     end
 
-    % Si no se introduce nada manejo los inputs por mi cuenta
-    if nargin < 2 || isempty(h_values)
-        h_values = 10.^(-(1:4)); 
+    if nargin < 2 || isempty(J_values)
+        J_values = [100];
     end
 
-    if nargin < 3 || isempty(k_values)
-        k_values = h_values; % Por defecto, k = h
+    if nargin < 3 || isempty(N_values)
+        % Condición de estabilidad (mu <= 1/2 asegurado)
+        % Uso 2.5 en vez de 2 ya que sino podria pasar qeu mu no este por
+        % debajo por errores de redondeo
+        N_values = 2.5*J_values.^2*T;
     end
 
     % Exigir el metodo y no crashear directamente si no se pasa nada
@@ -24,81 +35,91 @@ function [Times,Errors] = practica1_1(choiceMethod, h_values, k_values)
 
     method_names = {'Explícito', 'Implícito', 'Crank-Nicolson'};
 
-    % Dato del problema
-    T = 0.5;
 
-    % Condiciones iniciales y solución exacta
-    u0_fun = @(x) sin(pi*x);
-    u_exact = @(x,t) sin(pi*x) .* exp(-pi^2 * t);
+    % Inicialización de los valores de k, h 
+    L_j = length(J_values);
+    L_n = length(N_values); 
+    h_values = 1 ./ J_values;
+    k_values = T ./ N_values; 
+
 
     % Inicialización de matrices de resultados
-    L = length(k_values);
-    Times = zeros(L, L);
-    Errors = zeros(L, L);
+    Times = zeros(L_n, L_j);
+    Errors = zeros(L_n, L_j);
 
-    for i = 1:L
-        k = k_values(i);
-
-        for m = 1:L
-            h = h_values(m);
+    for n = 1:L_n
+        k = k_values(n);
+        N = N_values(n);
+        for j = 1:L_j
+            J = J_values(j);
+            h = h_values(j);
             mu = k / h^2;
-            J = ceil(1 / h);
-            N = ceil(T / k);
 
             % Malla
             t = linspace(0, T, N+1)';
             x = linspace(0, 1, J+1)';
-            x = x(2:end-1); 
+            x_int = x(2:end-1); 
 
             % Condición inicial
-            u0 = u0_fun(x);
+            u0 = u0_fun(x_int);
+
+            % Meter lo parametros en un struct para facilitar lectura.
+            params.J = J; 
+            params.N = N; 
+            params.mu = mu; 
+            params.x = x_int; 
+            params.t = t; 
 
             % Resolver
-            [time, errors, ~] = solver_selection(choiceMethod, J, N, mu, u0, x, t, u_exact);
+            [time, errors, ~] = solver_selection(choiceMethod, params, u0, u_exact);
 
             errmax = max(errors);
-            Times(i, m) = time;
-            Errors(i, m) = errmax;
+            Times(n, j) = time;
+            Errors(n, j) = errmax;
         end
     end
 
     nombre_metodo = method_names{choiceMethod};
 
     % 1) k fijo, variando h
-    for i = 1:L
-        subplot(ceil(L/2), 2, i);
-        loglog(Times(i,:), Errors(i,:), 'o-', 'LineWidth', 2, 'MarkerSize', 8);
+    figure(1);
+    for n = 1:L_n
+        subplot(ceil(L_n/2), 2, n);
+        loglog(Times(n,:), Errors(n,:), 'o-', 'LineWidth', 2, 'MarkerSize', 8);
         grid on;
         xlabel('Tiempo de ejecución');
         ylabel('Error máximo');
-        title(sprintf('k = %.0e fijo, variando h', k_values(i)));
+        title(sprintf('k = %.0e fijo, variando h', k_values(n)));
     end
+    print("-f1", "k_fijo_var_h_" + nombre_metodo, "-dpng")
 
     % 2) h fijo, variando k
-    for m = 1:L
-        subplot(ceil(L/2), 2, m);
-        loglog(Times(:,m), Errors(:,m), 's-', 'LineWidth', 2, 'MarkerSize', 8);
+    figure(2);
+    for j = 1:L_j
+        subplot(ceil(L_n/2), 2, j);
+        loglog(Times(:,j), Errors(:,j), 's-', 'LineWidth', 2, 'MarkerSize', 8);
         grid on;
         xlabel('Tiempo de ejecución');
         ylabel('Error máximo');
-        title(sprintf('h = %.0e fijo, variando k', h_values(m)));
+        title(sprintf('h = %.0e fijo, variando k', h_values(j)));
     end
-
+    print("-f2", "h_fijo_var_k_" + nombre_metodo, "-dpng")
     % 3) Eficiencia
+    figure(3); 
     loglog(Times(:), Errors(:), 'o', 'MarkerSize', 8);
     grid on;
-    xlabel('Tiempo de ejecución');
+    xlabel('Tiempo de cómputo (s)');
     ylabel('Error máximo');
     title(sprintf('Eficiencia: %s', nombre_metodo));
-
+    print("-f3", "eficiencia_" + nombre_metodo, "-dpng")
 end
 
-function [time, errors, u] = solver_selection(choice, j, n, mu, u0, x, t, u_exact)
-    switch choice
+function [time, errors, u] = solver_selection(choiceMethod, params, u0, u_exact)
+    switch choiceMethod
     case 1
-        if mu <= 1/2 
+        if params.mu <= 1/2 
             tic;
-            [u, errors] = solve_explicito(j, n, mu, u0, x, t, u_exact);
+            [u, errors] = solve_explicito(params, u0, u_exact);
             time = toc;
         else 
             errors(:) = NaN;
@@ -107,58 +128,76 @@ function [time, errors, u] = solver_selection(choice, j, n, mu, u0, x, t, u_exac
         end
     case 2
         tic;
-        [u, errors] = solve_implicito(j, n, mu, u0, x, t, u_exact);
+        [u, errors] = solve_implicito(params, u0, u_exact);
         time = toc;
     case 3
         tic;
-        [u, errors] = solve_crank(j, n, mu, u0, x, t, u_exact);
+        [u, errors] = solve_crank(params, u0, u_exact);
         time = toc;
     end
 end
 
-function [u, errors] = solve_explicito(j, n, mu, u, x, t, u_exact)
-    errors = zeros(1, n+1);
+function [u, errors] = solve_explicito(params, u, u_exact)
+    J = params.J;
+    N = params.N;
+    mu = params.mu;
+    x = params.x;
+    t = params.t;
 
-    A = spdiags([mu , (1-2*mu), mu], -1:1, j-1, j-1);
+    errors = zeros(1, N+1);
+
+    A = spdiags([mu , (1-2*mu), mu], -1:1, J-1, J-1);
 
     u_ex = u_exact(x, t(1));
     errors(1) = max(abs(u - u_ex));
 
-    for n = 1:n
+    for n = 1:N
         u = A * u;
         u_ex = u_exact(x, t(n+1));
         errors(n+1) = max(abs(u - u_ex));
     end
 end
 
-function [u, errors] = solve_implicito(j, n, mu, u, x, t, u_exact)
-    A = spdiags([-mu , (1+2*mu) , -mu ], -1:1, j-1, j-1);
+function [u, errors] = solve_implicito(params, u, u_exact)
+    J = params.J;
+    N = params.N;
+    mu = params.mu;
+    x = params.x;
+    t = params.t;
+
+    A = spdiags([-mu , (1+2*mu) , -mu ], -1:1, J-1, J-1);
     dA = decomposition(A);
 
-    errors = zeros(1, n+1);
+    errors = zeros(1, N+1);
 
     u_ex = u_exact(x, t(1));
     errors(1) = max(abs(u - u_ex));
 
-    for n = 1:n
+    for n = 1:N
         u = dA \ u;
         u_ex = u_exact(x, t(n+1));
         errors(n+1) = max(abs(u - u_ex));
     end
 end
 
-function [u, errors] = solve_crank(j, n, mu, u, x, t, u_exact)
+function [u, errors] = solve_crank(params, u, u_exact)
+    J = params.J;
+    N = params.N;
+    mu = params.mu;
+    x = params.x;
+    t = params.t;
+
     temp = mu / 2;
-    A = spdiags([-temp, (1+mu) , -temp ], -1:1, j-1, j-1);
-    B = spdiags([temp  (1-mu)  temp ], -1:1, j-1, j-1);
+    A = spdiags([-temp, (1+mu) , -temp ], -1:1, J-1, J-1);
+    B = spdiags([temp  (1-mu)  temp ], -1:1, J-1, J-1);
     dA = decomposition(A);
 
-    errors = zeros(1, n+1);
+    errors = zeros(1, N+1);
 
     u_ex = u_exact(x, t(1));
     errors(1) = max(abs(u - u_ex));
 
-    for n = 1:n
+    for n = 1:N
         u = dA \ (B * u);
         u_ex = u_exact(x, t(n+1));
         errors(n+1) = max(abs(u - u_ex));
